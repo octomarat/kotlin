7f4a475c960365521c43b11f8a96cfa802998786
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-06-30 19:28:57 +0300

    Tests for targeted annotations (@field, @file fixes)

---
+++ compiler/testData/diagnostics/tests/annotations/FieldAnnotations.kt
@@ -0,0 +1,31 @@
+annotation class Ann
+
+class CustomDelegate {
+    public fun get(thisRef: Any?, prop: PropertyMetadata): String = prop.name
+}
+
+<!INAPPLICABLE_FIELD_TARGET!>@field:Ann<!>
+class SomeClass {
+
+    @field:Ann
+    protected val simpleProperty: String = "text"
+
+    @field:[Ann]
+    protected val simplePropertyWithAnnotationList: String = "text"
+
+    <!FIELD_TARGET_NO_BACKING_FIELD!>@field:Ann<!>
+    protected val delegatedProperty: String by CustomDelegate()
+
+    <!FIELD_TARGET_NO_BACKING_FIELD!>@field:Ann<!>
+    val propertyWithCustomGetter: Int
+        get() = 5
+
+    <!INAPPLICABLE_FIELD_TARGET!>@field:Ann<!>
+    fun annotationOnFunction(a: Int) = a + 5
+
+    fun anotherFun() {
+        <!INAPPLICABLE_FIELD_TARGET!>@field:Ann<!>
+        val <!UNUSED_VARIABLE!>localVariable<!> = 5
+    }
+
+}
