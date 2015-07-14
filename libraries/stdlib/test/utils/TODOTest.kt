74450e31d407f02ee71855bafdd01ce1aa7c3a83
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-07-14 19:57:15 +0300

    TODO as a function. Add an overload with a string reason. #KT-8153

--- libraries/stdlib/test/utils/TODOTest.kt
+++ libraries/stdlib/test/utils/TODOTest.kt
@@ -5,17 +5,17 @@ import kotlin.test.*
 import org.junit.Test as test
 
 private class PartiallyImplementedClass {
-    public val prop: String get() = TODO
+    public val prop: String get() = TODO()
 
-    internal fun method1() = TODO as String
-    public fun method2(): Int = TODO
+    internal fun method1() = TODO() as String
+    public fun method2(): Int = TODO()
 
     public fun method3(switch: Boolean, value: String): String {
         if (!switch)
-            TODO
+            TODO("what if false")
         else {
             if (value.length() < 3)
-                throw TODO
+                throw TODO("write message")
             else
                 return value
         }
@@ -23,14 +23,25 @@ private class PartiallyImplementedClass {
 }
 
 class TODOTest {
+    private fun assertNotImplemented(block: () -> Unit) {
+        assertTrue(fails(block) is UnsupportedOperationException)
+    }
+
+    private fun assertNotImplementedWithMessage(message: String, block: () -> Unit) {
+        val e = fails(block)
+        assertTrue(e is UnsupportedOperationException)
+        assertTrue(message in e!!.getMessage()!!)
+    }
+
+
     test fun usage() {
         val inst = PartiallyImplementedClass()
 
-        assertTrue(fails { inst.prop } is UnsupportedOperationException)
-        assertTrue(fails { inst.method1() } is UnsupportedOperationException)
-        assertTrue(fails { inst.method2() } is UnsupportedOperationException)
-        assertTrue(fails { inst.method3(false, "test") } is UnsupportedOperationException)
-        assertTrue(fails { inst.method3(true, "t") } is UnsupportedOperationException)
+        assertNotImplemented { inst.prop }
+        assertNotImplemented{ inst.method1() }
+        assertNotImplemented { inst.method2() }
+        assertNotImplementedWithMessage("what if false") { inst.method3(false, "test") }
+        assertNotImplementedWithMessage("write message") { inst.method3(true, "t") }
         assertEquals("test", inst.method3(true, "test"))
     }
 }
